b0ef16194a804a57b931e1f577c749db22b5b86f
m1ke <git@m1ke.me>
2015-02-18 15:08:16 +0000

    Change to example php

--- example.php
+++ example.php
@@ -13,7 +13,9 @@ $db_compare->addDb('db_1')->addDb('db_2');
 $output=$db_compare->compare('table',['key1','key2'],'primary_key');
 
 /* results in array [
-	'table' => array with one "row" per "row with differences". Each row contains 1 column per database per key
-	'arr' => 3d array with one "row" per "row with differences". Each row contains one array per key, with each array containing a value for each database name
+	'table' => array with one "row" per "row with differences".
+		Each row contains 1 column per database per key
+	'arr' => 3d array with one "row" per "row with differences".
+		Each row contains one array per key, with each array containing a value for each database name
 	'new' => array where the keys are primary keys of new rows, contains no data
 ]
